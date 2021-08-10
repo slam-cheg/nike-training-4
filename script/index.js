b645ec4ce0584d348cd032cfc16d2f5fb0d7bbc9
Lev Shushkov <levashushkov@yandex.ru>
2021-08-11 01:29:46 +0300

    2+3
    Блок о коллекции, блок о товарах десктоп верстка. Начато написание слайдера для товаров. Пока он не работает, ошибки не выдает. Нужно поправить

--- script/index.js
+++ script/index.js
@@ -1,6 +1,27 @@
 const burger = document.querySelector('.header__burger');
 const closeButton = document.querySelector('.header__menu-icon');
 const menuCover = document.querySelector('.header__menu');
+const sliderTrack = document.querySelector('.slider__track');
+const nextButton = document.querySelector('.slider__arrow_right');
+const prevButton = document.querySelector('.slider__arrow_left');
+let offset = 0;
+
+nextButton.addEventListener('click', function () {
+    offset = offset + 326;
+    if (offset > 1304) {
+        offset = 0;
+    }
+    sliderTrack.style.left = -offset + 'px';
+});
+
+prevButton.addEventListener('click', function () {
+    offset = offset - 326;
+    if (offset < 0) {
+        offset = 1304;
+    }
+    sliderTrack.style.left = -offset + 'px';
+});
+
 
 burger.addEventListener('click', function () {
     openMenu()
@@ -16,3 +37,4 @@ function openMenu() {
 function closeMenu() {
     menuCover.classList.remove('header__menu_opened');
 }
+
