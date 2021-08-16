60a980bfa042a184acaa81c8f6fadb2154e11cf6
Lev Shushkov <levashushkov@yandex.ru>
2021-08-16 20:13:15 +0300

    desktop final

--- script/index.js
+++ script/index.js
@@ -4,22 +4,31 @@ const menuCover = document.querySelector('.header__menu');
 const sliderTrack = document.querySelector('.slider__track');
 const nextButton = document.querySelector('.slider__arrow_right');
 const prevButton = document.querySelector('.slider__arrow_left');
+const card = document.querySelector('.slider__card');
+const cardWidth = card.offsetWidth;
+
 let offset = 0;
 
 nextButton.addEventListener('click', function () {
-    offset = offset + 326;
-    if (offset > 1304) {
+    offset = offset + cardWidth;
+    
+    if (offset > (cardWidth * 2)) {
         offset = 0;
     }
+
     sliderTrack.style.left = -offset + 'px';
+
 });
 
 prevButton.addEventListener('click', function () {
-    offset = offset - 326;
+    offset = offset - cardWidth;
+
     if (offset < 0) {
-        offset = 1304;
+        offset = (cardWidth * 2);
     }
+
     sliderTrack.style.left = -offset + 'px';
+
 });
 
 
